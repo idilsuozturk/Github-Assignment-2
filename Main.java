f8a36a047b50a33937197807e824d80cf55b1679
idilsuozturk <99552612+idilsuozturk@users.noreply.github.com>
2023-02-15 22:59:47 +0300

    Added main class
    added main class to display the menu, there will be additions after new methods

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Please enter your choice: ");
+        System.out.println("1-Display array" +
+                           "2-Find min value of the array" +
+                           "3-Find max value of the array" +
+                           "4-");
+    }
+}
